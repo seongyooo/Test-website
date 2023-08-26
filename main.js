8b9a77be149d61e6ac2504b961c0a2d288e09880
seongyooo <seonlineseon@gmail.com>
2023-08-26 14:46:27 +0900

    login make

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-for (let i = 0; i < 10; i++)
-  console.log(alert(i));
+let user;
+let passWord="TheMaster";
+
+user = prompt("User name", '');
+
+if(!user)
+  alert("취소되었습니다");
+else if(user !== passWord)
+  alert("인증에 실패하였습니다");
+else
+  alert("환영합니다!");
+  
+  
