75d62751fe6166b1b285afa1fb6b42bef641c22c
seongyooo <seonlineseon@gmail.com>
2023-08-26 15:11:03 +0900

    change struct

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let user;
 let passWord="TheMaster";
+let check=0;
 
 user = prompt("User name", '');
 
 
-
-if(!user || user !== "Admin")
-  alert("취소되었습니다");
-else if(user !== passWord)
-  alert("인증에 실패하였습니다");
+if(user === Admin){
+  check = passWord === prompt("비밀번호 입력", '');
+  
+  if(check)
+    alert("환영합니다!");
+  else if(!check)
+    alert("인증에 실려하다습니다");
+}
 else
-  alert("환영합니다!");
+  alert("취소되었습니다");
+
+
   
   
