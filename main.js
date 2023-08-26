41b6d1acfbc238b6c27718a08c8e6c0308aa90ed
seongyooo <seonlineseon@gmail.com>
2023-08-26 15:16:04 +0900

    add if

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ user = prompt("User name", '');
 
 
 if(user === Admin){
-  check = (passWord === prompt("비밀번호 입력", ''));
+  check = prompt("비밀번호 입력", '');
   
-  if(check)
+  if(check === passWord)
     alert("환영합니다!");
-  else if(!check)
-    alert("인증에 실려하다습니다");
+  else if(check !== passWord)
+    alert("인증에 실패하였습니다");
 }
 else
   alert("취소되었습니다");
